bb091673d26bc98484d1f95260a3968d9f71f2b8
Pape Samba SIDIBE <papesambas@yahoo.fr>
2024-07-03 21:48:50 +0200

    first commit

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,16 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import boostrap from 'bootstrap';
+import canvasconfetti from 'canvas-confetti';
+//import jquery from 'jquery';
+// import Jquery
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 import './styles/app.css';
+
+document.body.addEventListener('click', ()=>{
+    canvasconfetti()
+})
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- importmap.php
+++ importmap.php
@@ -25,4 +25,20 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'bootstrap' => [
+        'version' => '5.3.3',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
+    'canvas-confetti' => [
+        'version' => '1.9.3',
+    ],
+    'jquery' => [
+        'version' => '3.7.1',
+    ],
 ];
